6c29466387859bd026bc220c08ce046ef391b3c9
Raquel Rosales <lraquelr6@gmail.com>
2023-05-13 02:51:20 -0400

    first-commit

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -29,14 +29,41 @@ def sitemap():
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    try:
+        members = jackson_family.get_all_members()
+        response_body = members
+        return jsonify(response_body), 200
+    except:
+        return jsonify({"msg":"Bad Request"}),400
 
 
-    return jsonify(response_body), 200
+@app.route('/member',methods=['POST'])
+def add_member():
+    try:
+        request_body = json.loads(request.data)
+        jackson_family.add_member(request_body)
+        return jsonify(request_body),200
+    except:
+        return jsonify({"msg":"Bad Request"}),400
+
+    
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    try:
+        jackson_family.delete_member(id)
+        return jsonify({"done":True}),200
+    except:
+        return jsonify({"msg":"Bad Request"}),400
+
+
+@app.route('/member/<int:id>')
+def get_member(id):
+    try:
+        member=jackson_family.get_member(int(id))
+        return jsonify(member),200
+    except Exception as e:
+        return jsonify({"msg":"Bad Request"}),400
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -13,23 +12,35 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+		 "id": self._generateId(), "first_name":"Jhon", "age": 33, "lucky_numbers": [7, 13, 22]},
+            {"id": self._generateId(), "first_name":"Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
+            {"id": self._generateId(), "first_name":"Jimmy", "age": 5, "lucky_numbers": [1]
+	}
+    ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
+        self._members.append(member)
         # fill this method and update the return
-        pass
+        return "Miembre Agregado"
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for i, item in enumerate(self._members, start=0):
+            if item["id"] == id:
+                self._members.pop(i)
+        return "borrado"
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for i in range(len(self._members)-1):
+            print(i)
+            if id==self._members[i]["id"]:
+                return self._members[i]
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
